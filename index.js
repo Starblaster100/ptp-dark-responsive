7b5be79910f108b896512ac18585457760d69ce3
Starblaster100 <starblaster100@gmail.com>
2021-02-20 18:57:46 -0800

    🔐 Enabling https via webpack-dev-server

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const path = require('path');
+const https = require('https');
 const express = require('express');
+const getCertificate = require('webpack-dev-server/lib/utils/getCertificate');
 
 const app = express();
 const port = 3000;
+const fakeCert = getCertificate({ info: () => {} });
+
+const options = {
+  key: fakeCert,
+  cert: fakeCert,
+};
 
 app.use(express.static('dist'));
 
@@ -10,6 +17,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// start server
+const server = https.createServer(options, app);
+server.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('server starting on port: 3000');
 });
